7efc1a3ee0b8a0b8e870ad5c86bbc4729ace9658
liu-kai-github <liu-kai-github@users.noreply.github.com>
2018-05-18 14:05:06 +0800

    添加路由

--- src/index.js
+++ src/index.js
@@ -1,8 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import Index from './views/App';
-import registerServiceWorker from './registerServiceWorker';
+import {createBrowserHistory} from "history";
+import {Router, Route, Switch} from "react-router";
 
-ReactDOM.render(<Index />, document.getElementById('root'));
+import indexRoutes from "src/routes/index.jsx";
+import './index.css';
+import registerServiceWorker from './registerServiceWorker';
+const hist = createBrowserHistory();
+
+ReactDOM.render(
+    <Router history={hist}>
+        <Switch>
+            {indexRoutes.map((prop, key) => {
+                return (
+                    <Route
+                        path={prop.path}
+                        key={key}
+                        component={prop.component}
+                    />
+                );
+            })}
+        </Switch>
+    </Router>,
+    document.getElementById('root'));
 registerServiceWorker();
---
+++ src/routes/index.jsx
@@ -0,0 +1,8 @@
+import App from "src/views/App";
+
+const indexRoutes = [
+    {path: "/app", name: "app", component: App},
+    {path: "/", name: "app", component: App}
+];
+
+export default indexRoutes;
