6f1d1b1b1e4199e04214d1bf56ea49f0fff07758
liu-kai-github <liu-kai-github@users.noreply.github.com>
2018-05-23 17:55:42 +0800

    更新路由代码

--- src/index.js
+++ src/index.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {createBrowserHistory} from "history";
-import {Router, Route, Switch} from "react-router";
+
+// import {createBrowserHistory} from "history";
+// import {Router, Route, Switch} from "react-router";
+// const hist = createBrowserHistory();
+
+import {
+    // BrowserRouter as Router,
+    HashRouter as Router,
+    Switch,
+    Route,
+} from "react-router-dom";
 
 import indexRoutes from "src/routes/index.jsx";
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
-const hist = createBrowserHistory();
 
 ReactDOM.render(
-    <Router history={hist}>
+    <Router>
         <Switch>
             {indexRoutes.map((prop, key) => {
                 return (
